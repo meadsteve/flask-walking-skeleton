b74ca72cb4f91279937262b62972051124f1164c
steve <meadsteve@gmail.com>
2020-05-08 21:08:52 +0200

    use jordimartin/mmock to fake a remote service

--- skeleton/app.py
+++ skeleton/app.py
@@ -1,6 +1,14 @@
+import os
+
+import requests
 from flask import Flask
+
+REMOTE_SERVICE_URL = os.environ['REMOTE_SERVICE_URL']
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    response = requests.get(REMOTE_SERVICE_URL + "/hello")
+    return f"got response from other server: {response.content}"
